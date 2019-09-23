af0693f42f0d98ea4155902044f79974180ba1ae
Alyssa Farina <alyssafarina695@gmail.com>
2019-09-23 09:19:58 -0400

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,22 +3,35 @@ import StockContainer from './StockContainer'
 import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
+const API_URL = 'http://localhost:3000/stocks'
+
 class MainContainer extends Component {
+
+  state = {
+    stocks: [],
+    portfolio: [],
+    filter: 'All',
+    sortBy: ''
+  }
+
+  componentDidMount() {
+    this.fetchStocks();
+  }
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar changeFilter={this.changeFilter} changeSort={this.changeSort} sortBy={this.state.sortBy}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.stocks} filter={this.state.filter} sortBy={this.state.sortBy} addStockToPortfolio={this.addStockToPortfolio}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer portfolio={this.state.portfolio} removeStockFromPortfolio={this.removeStockFromPortfolio}/>
 
             </div>
           </div>
@@ -26,6 +39,49 @@ class MainContainer extends Component {
     );
   }
 
+  fetchStocks = () => {
+    fetch(API_URL)
+      .then(response=>response.json())
+      .then(stocksArray => {
+        this.setState({
+          stocks: stocksArray
+        })
+      })
+  }
+
+  addStockToPortfolio = (e) => {
+    let newPortfolio = [...this.state.portfolio]
+    let stockToBuy = this.state.stocks.find(stock => {
+      return stock.id === parseInt(e.target.id, 10); 
+    });
+    newPortfolio.push(stockToBuy)
+    this.setState({
+      portfolio: newPortfolio
+    })
+  }
+
+  removeStockFromPortfolio = (e) => {
+    let newPortfolio = this.state.portfolio
+    let stockToSellIndex = this.state.portfolio.findIndex(stock => {
+      return stock.id === parseInt(e.target.id, 10);
+    });
+    newPortfolio.splice(stockToSellIndex,1)
+    this.setState({
+      portfolio: newPortfolio
+    })
+  }
+
+  changeFilter = (event) => {
+    this.setState({
+      filter: event.target.value
+    })
+  }
+
+  changeSort = (event) => {
+    this.setState({
+      sortBy: event.target.value
+    })
+  }
 }
 
 export default MainContainer;
